71236c2b99c9d9fe3d889242a68985cdf9560ae4
Andre Luckow <andre.luckow@gmail.com>
2018-01-15 10:24:58 -0600

    added example

--- mass/LightSourceKafkaProducer.py
+++ mass/LightSourceKafkaProducer.py
@@ -120,6 +120,69 @@ def produce_block_kmeans(block_id=1,
     }
     return stats 
 
+
+def produce_block_kmeans(block_id=1,
+                  kafka_zk_hosts=None,
+                  number_clusters_per_partition=NUMBER_CLUSTER,
+                  number_points_per_cluster=NUMBER_POINTS_PER_CLUSTER,
+                  number_points_per_message = NUMBER_POINTS_PER_MESSAGE,
+                  number_dim=NUMBER_DIM,
+                  topic_name=TOPIC_NAME):
+    start = time.time()
+    num_messages = 0
+    count_bytes  = 0
+    count = 0
+    
+    print "Zookeeper: %s, Block Id: %s, Num Cluster: %d" % (kafka_zk_hosts, str(block_id), NUMBER_CLUSTER)
+
+    # init Kafka
+    client = KafkaClient(zookeeper_hosts=kafka_zk_hosts)
+    topic = client.topics[topic_name]
+    producer = topic.get_sync_producer(partitioner=hashing_partitioner)
+    
+    if producer is None: print "Producer None"; return -1
+    
+    # partition on number clusters
+    points = []
+    for i in range(number_clusters_per_partition):    
+        p = get_random_cluster_points(number_points_per_cluster, number_dim)
+        points.append(p)
+    points_np=np.concatenate(points)
+    
+    number_messages = points_np.shape[0]/number_points_per_message
+    end_data_generation = time.time()
+    print "Points Array Shape: %s, Number Batches: %.1f"%(points_np.shape, number_messages)
+
+    last_index=0
+    count_bytes = 0
+    for i in range(number_messages):
+        logging.debug("Messages#: %d, Points: %d - %d, Points/Message: %d, KBytes: %.1f, KBytes/sec: %s"%\
+                                     (num_messages+1,
+                                      last_index,                                                                                           
+                                      last_index+number_points_per_message, 
+                                      
+                                      number_points_per_message,
+                                      count_bytes/1024,
+                                      count_bytes/1024/(time.time()-end_data_generation))) 
+        points_batch = points_np[last_index:last_index+number_points_per_message]
+        points_strlist=str(points_batch.tolist())
+        producer.produce(points_strlist, partition_key='{}'.format(count))
+        count = count + 1
+        last_index = last_index + number_points_per_message
+        count_bytes = count_bytes + len(points_strlist)
+        num_messages = num_messages + 1
+    end = time.time()
+    stats = {
+        "block_id": block_id,
+        "number_messages" :  num_messages,
+        "points_per_message": number_points_per_message,
+        "bytes_per_message": str(len(points_strlist)),
+        "data_generation_time": "%5f"%(end_data_generation-start),
+        "transmission_time":  "%.5f"%(end-end_data_generation),
+        "runtime": "%.5f"%(end-start)
+    }
+    return stats 
+
 ######################################################################################
 # Light Source
 
@@ -193,7 +256,7 @@ class MiniApp():
         
         
         if self.application_type == "kmeans":
-            self.number_messages = (self.number_points_per_cluster * self.number_cluster)/self.number_points_per_message
+            self.number_messages = (self.number_points_per_cluster * self.number_clusters)/self.number_points_per_message
         elif self.application_type == "light":
             self.number_messages = number_messages
             self.number_total_points = number_messages # 1 message contains 1 point (image)
