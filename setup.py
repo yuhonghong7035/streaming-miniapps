c92785b68f2b236d610ce1e8c029116fe95a0477
George Chantzialexiou <george.chantzialexiou@gmail.com>
2018-05-21 12:49:27 -0500

    adding the data file of cms generator to the mini-app module

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(name='Streaming Mini Apps',
             #     "spark", "commandline"],
 
       # data files for easy_install
-      package_data= {'': ['*.h5', '*.xml', '*.yaml', '*.properties']},
+      package_data= {'': ['*.h5', '*.xml', '*.yaml', '*.properties','*.hd5']},
       install_requires=['pykafka', "setuptools-git" ],
       #entry_points = {
       #  'console_scripts': ['streaming-miniapp=commandline.main:main',
